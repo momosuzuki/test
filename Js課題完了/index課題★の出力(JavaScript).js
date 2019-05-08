6e3e554e712eed25a8b61defddf47d6f13a981f6
momosuzuki <momoxdora090393@gmail.com>
2019-05-08 21:26:46 +0900

    test version01

---
+++ Js課題完了/index課題★の出力(JavaScript).js
@@ -0,0 +1,72 @@
+for(var i =1;i<=5;i++){
+    document.write("★")
+}
+document.write("<br>")
+document.write("<br>")
+for(var i =1;i<=6;i++){
+    document.write("★")
+	if(i==3){document.write("<br>");}
+}
+document.write("<br>")
+document.write("<br>")
+for(var i =1;i<=10;i++){
+	document.write("☆")
+	if(i==5){document.write("<br>");}
+}
+document.write("<br>")
+document.write("<br>")
+for(var i =1;i<=20;i++){
+	document.write("★")
+	if(i==5){document.write("<br>");}
+	if(i==10){document.write("<br>");}
+	if(i==15){document.write("<br>");}
+}
+document.write("<br>")
+document.write("<br>")
+
+for(var i =1;i<=12;i++){
+	document.write("★")
+	if(i==3){document.write("<br>");}
+	if(i==6){document.write("<br>");}
+	if(i==9){document.write("<br>");}
+}
+document.write("<br>")
+document.write("<br>")
+for(var i =1;i<=3;i++){
+		for(var j =1;j<=3;j++){
+			if(j==2){
+			   document.write("☆")
+			}else{
+				document.write("★")
+			}
+			
+		}
+	document.write("<br>")
+}
+
+document.write("<br>")
+document.write("<br>")
+
+for(var i =1;i<=4;i++){
+	for(var j =1;j<=5;j++){
+		if(j%2==0){
+			document.write("☆")
+		}else{
+			document.write("★")
+		}
+	}
+document.write("<br>")}
+document.write("<br>")
+document.write("<br>")
+    
+for(var i =1;i<=5;i++){
+	for(var j =1;j<=i;j++){
+			document.write("★")
+    }
+    document.write("<br>")
+}
+        
+        
+        
+        
+        
